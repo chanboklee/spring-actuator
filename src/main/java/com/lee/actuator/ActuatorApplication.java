b964e5d56cc747cf295056968e257e88357b0cea
chanboklee <chanboklee@naver.com>
2023-04-24 00:43:00 +0900

    Fix : spring-actuator - add metric sample

--- src/main/java/com/lee/actuator/ActuatorApplication.java
+++ src/main/java/com/lee/actuator/ActuatorApplication.java
@@ -1,11 +1,14 @@
 package com.lee.actuator;
 
+import com.lee.actuator.order.v0.OrderServiceV0;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import(OrderServiceV0.class)
+@SpringBootApplication(scanBasePackages = "com.lee.actuator.controller")
 public class ActuatorApplication {
 
     public static void main(String[] args) {
